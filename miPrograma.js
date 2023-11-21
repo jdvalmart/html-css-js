4ad179958d483f1134426d38d76a50cf0053f3b9
jdvalmart <juanvalencia9411@outlook.com>
2023-11-21 00:59:11 -0500

    fin de ejercicio

---
+++ miPrograma.js
@@ -0,0 +1,20 @@
+window.addEventListener("load",asignarEventos,false);
+
+function asignarEventos(){
+    document.getElementById("menu1").addEventListener("click",click1,false);
+    document.getElementById("menu2").addEventListener("click",click2,false);
+    document.getElementById("menu3").addEventListener("click",click3,false);
+}
+
+function click1(){
+    window.alert("hizo clic en el menu 1");
+}
+
+function click2(){
+    window.alert("hizo clic en el menu 2");
+}
+
+function click3(){
+    window.alert("hizo clic en el menu 3");
+}
+
